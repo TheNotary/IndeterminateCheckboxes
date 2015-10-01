4e48e219e70ded3023864e75830013f240c0263d
TheNotary <no@email.plz>
2015-10-01 16:09:21 -0700

    finished sorting out logic for recursive function

--- indeterminateCheckbox.js
+++ indeterminateCheckbox.js
@@ -5,57 +5,60 @@ $(function() {
             container = getListItemContainingCheckbox($(this));
 
         markAnyChildrenToShareTheCheckStateOfTarget(container, checkStateOfTarget);
-
         checkRelatives(container);
 
         // Closures:  checked,
+        // This recursion based function will check if the siblings of el
+        // all match the entering target checkbox's state, and when they do
+        // it set's the parent's check state accordingly...
+        // Then it bubbles up to the parent's tier until it reaches a point
+        // of bubbling where the el is NOT an LI element.
         //
+        // el should be a container for an inputbox (li)
         function checkRelatives(el) {
+            // Prevent bubbling up beyond the treeview
+            if (el.prop("tagName") != "LI") return;
+
             var parent = el.parent().parent();
+            var doTargetSiblingsOrParentSiblingsMatchTargetCheckState = checkIfSiblingsToTargetOrParentsAreTheSameCheckStateAsTarget(el);
 
-            var doTargetOrParentSiblingsMatchTargetCheckState = checkIfSiblingsToTargetOrParentsAreTheSameCheckStateAsTarget(el);
-
-            if (allCheckboxesOfThisTierAreChecked()) {
-                parent.children('input[type="checkbox"]').prop({
-                    indeterminate: false,
-                    checked: checkStateOfTarget
-                });
+            if (allSiblingsOfThisTierAreChecked()) {
+                markTheParentCheckboxToThisContainersTierAsChecked(parent);
                 checkRelatives(parent);
-            } else if (allCheckboxesOfThisTierAreUnchecked()) {
-                parent.children('input[type="checkbox"]').prop("checked", checkStateOfTarget);
-                parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
+            }
+            else if (allCheckboxesOfThisTierAreUnchecked()) {
+                markTheParentCheckboxToThisContainersTierAsUnchecked(parent);
                 checkRelatives(parent);
-            } else { // the checkboxes of this tier are mix and matched
-                console.log("mix matched");
-                el.parents("li").children('input[type="checkbox"]').prop({
-                    indeterminate: true,
-                    checked: false
-                });
+            }
+            // The checkboxes of this tier are mix and matched, therefore
+            // we need to go up to every direct ancestor of this tier and set indeterminate
+            else {
+                markAllDirectAncestorsAsIndeterminate(el);
             }
 
+            // /End confusing recursive logic
 
-            function allCheckboxesOfThisTierAreChecked() {
-                return (doTargetOrParentSiblingsMatchTargetCheckState && checkStateOfTarget);
+            // returns true if all the siblings of the specified element are of
+            // the same checkstate as the target originally clicked
+            function allSiblingsOfThisTierAreChecked() {
+                return (doTargetSiblingsOrParentSiblingsMatchTargetCheckState && checkStateOfTarget);
             }
 
             function allCheckboxesOfThisTierAreUnchecked() {
-                return (doTargetOrParentSiblingsMatchTargetCheckState && !checkStateOfTarget);
+                return (doTargetSiblingsOrParentSiblingsMatchTargetCheckState && !checkStateOfTarget);
             }
 
             function checkIfSiblingsToTargetOrParentsAreTheSameCheckStateAsTarget(el) {
-                var doTargetOrParentSiblingsMatchTargetCheckState = true;
+                var doTargetSiblingsOrParentSiblingsMatchTargetCheckState = true;
 
                 el.siblings().each(function() {
-                    doTargetOrParentSiblingsMatchTargetCheckState = ($(this).children('input[type="checkbox"]').prop("checked") === checkStateOfTarget);
+                    doTargetSiblingsOrParentSiblingsMatchTargetCheckState = ($(this).children('input[type="checkbox"]').prop("checked") === checkStateOfTarget);
                 });
 
-                return doTargetOrParentSiblingsMatchTargetCheckState;
+                return doTargetSiblingsOrParentSiblingsMatchTargetCheckState;
             }
 
         }
-
-
-
 
         function getListItemContainingCheckbox(checkboxEl) {
             return checkboxEl.parent();
@@ -70,8 +73,30 @@ $(function() {
             });
         }
 
+        // When Andre is un-ticked, the first occurance of this el is
+        // the li container to Andre's textbox/ label.
+        // The parents("li") are actually the containers to Giants and Tall Things
+        // So this sets the direct ancestors to an indeterminate state
+        function markAllDirectAncestorsAsIndeterminate(elContainer) {
+            elContainer.parents("li").children('input[type="checkbox"]').prop({
+                indeterminate: true,
+                checked: false
+            });
+        }
 
+        function markTheParentCheckboxToThisContainersTierAsUnchecked(parent) {
+            parent.children('input[type="checkbox"]').prop({
+                checked: false,
+                indeterminate: (parent.find('input[type="checkbox"]:checked').length > 0)
+            });
+        }
 
+        function markTheParentCheckboxToThisContainersTierAsChecked(parent) {
+            parent.children('input[type="checkbox"]').prop({
+                indeterminate: false,
+                checked: true
+            });
+        }
 
 
     });
